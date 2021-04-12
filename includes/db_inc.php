f0aef9fdbeb4ec385ac3c2ff455a7e13992bd22a
Fery <emailferyy@gmail.com>
2021-04-12 20:59:34 +0700

    updated configuration

--- includes/db_inc.php
+++ includes/db_inc.php
@@ -1,10 +1,9 @@
 <?php
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$servername = "cleardb_url['host']";
-$dbusername = "cleardb_url['user']";
-$dbpassword = "cleardb_url['pass']";
-$dbname = substr($cleardb_url["path"],1);
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "uts_vaksinasi");
 
 $connection = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
 
